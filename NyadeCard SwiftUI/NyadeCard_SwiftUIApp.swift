c167bd6e516d2b430703e84a174703c178828cce
Nyade <niinyade@icloud.com>
2022-06-14 17:06:05 -0300

    Initial Commit

---
+++ NyadeCard SwiftUI/NyadeCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyadeCard_SwiftUIApp.swift
+//  NyadeCard SwiftUI
+//
+//  Created by Nyade on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NyadeCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
